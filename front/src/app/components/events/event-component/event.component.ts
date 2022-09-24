d96365d703720a3ad89bf8b680d5c97d5d342dcb
mariniuc <dan_mariniuc@yahoo.com>
2022-09-24 22:58:34 +0300

    fixed front

---
+++ front/src/app/components/events/event-component/event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.scss']
+})
+export class EventDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
